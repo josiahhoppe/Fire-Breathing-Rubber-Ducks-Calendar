7d326e106d8c8b2ec0f532f1fc0dd373c27344ef
josiahhoppe <jhoppe@blc.edu>
2024-11-13 12:01:57 -0600

    Add files via upload

---
+++ Generic Tester.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+using AppointmentNameSpace;
+
+public class GenericTester
+{
+        public static void Main()
+        {
+        AppointmentMenu myMenu = new AppointmentMenu();
+            myMenu.Run();
+        }
+}
